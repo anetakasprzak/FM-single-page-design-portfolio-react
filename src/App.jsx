4765a7246bcd353973e119861cae95777568cba2
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-07 11:32:30 +0000

    tidy up the code

--- src/App.jsx
+++ src/App.jsx
@@ -26,138 +26,16 @@ const data = [
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isThankyouOpen, setIsThankyouOpen] = useState(false);
+  const [name, setName] = useState("");
 
   return (
     <div className="wrapper">
       <div className="main__component">
-        <section className="heading">
-          <div className="logo__box">
-            <img src="../public/logo.svg" className="logo" alt="logo" />
-            <button
-              className="btn btn--black"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Free Consultation
-            </button>
-          </div>
-          <div className="header__box">
-            <h1 className="header">Design solutions made easy</h1>
-            <p className="text">
-              With over ten years of experience in various design disciplines,
-              I&apos;m your one-stop shop for your design needs.
-            </p>
-          </div>
-        </section>
-
-        <section className="grid">
-          <div className="grid--el grid--1">
-            <img
-              className="grid__img"
-              src="../public/pattern-graphic-design.svg"
-              alt="image"
-            />
-            <p className="grid__text">Graphic Design</p>
-          </div>
-
-          <div className="grid--el grid--2">
-            <img
-              className="grid__img"
-              src="../public/pattern-ui-ux.svg"
-              alt="image"
-            />
-            <p className="grid__text">UI/UX</p>
-          </div>
-
-          <div className="grid--el grid--3">
-            <img
-              className="grid__img"
-              src="../public/pattern-apps.svg"
-              alt="image"
-            />
-            <p className="grid__text">Apps</p>
-          </div>
-
-          <div className="grid--el grid--4">
-            <img
-              className="grid__img"
-              src="../public/pattern-illustrations.svg"
-              alt="image"
-            />
-            <p className="grid__text">Illustrations</p>
-          </div>
-
-          <div className="grid--el grid--5">
-            <img
-              className="grid__img"
-              src="../public/pattern-photography.svg"
-              alt="image"
-            />
-            <p className="grid__text">Photography</p>
-          </div>
-
-          <div className="grid--el grid--6">
-            <img
-              className="grid__img"
-              src="../public/pattern-motion-graphics.svg"
-              alt="image"
-            />
-            <p className="grid__text">Motion Graphics</p>
-          </div>
-        </section>
-
-        <section className="amy__about">
-          <img
-            src="../public/image-amy.webp"
-            alt="amy img"
-            className="amy__img"
-          />
-          <div className="amy__text-box">
-            <h2 className="amy__heading">
-              I&apos;m Amy, and I&apos;d love to work on your next project
-            </h2>
-            <p className="amy__text">
-              I love working with others to create beautiful design solutions.
-              I&apos;ve designed everything from brand illustrations to complete
-              mobile apps. I&apos;m also handy with a camera!
-            </p>
-            <button
-              className="btn btn--orange"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Free Consultation
-            </button>
-          </div>
-        </section>
-
+        <Heading setIsModalOpen={setIsModalOpen} />
+        <Grid />
+        <AboutAmy setIsModalOpen={setIsModalOpen} />
         <Slider slides={data} />
-
-        <section className="cta">
-          <div className="cta__box">
-            <div className="cta__text-box">
-              <h2 className="cta__heading">Book a call with me</h2>
-              <p className="cta__text">
-                I&apos;d love to have a chat to see how I can help you. The best
-                first step is for us to discuss your project during a free
-                consultation. Then we can move forward from there.
-              </p>
-            </div>
-            <button
-              className="btn btn--orange"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Free Consultation
-            </button>
-          </div>
-          <div className="logo__box">
-            <img src="../public/logo.svg" className="logo" alt="logo" />
-            <button
-              className="btn btn--black"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Free Consultation
-            </button>
-          </div>
-        </section>
+        <Cta setIsModalOpen={setIsModalOpen} />
       </div>
 
       {isModalOpen && (
@@ -172,11 +50,21 @@ export default function App() {
                   >
                     Cancel
                   </button>
-                  <form className="form__box">
+                  <form
+                    className="form__box"
+                    onSubmit={(e) => {
+                      setTimeout(() => {
+                        e.preventDefault();
+                        console.log("called");
+                      }, 500);
+                    }}
+                  >
                     <input
                       className="input"
                       type="text"
                       placeholder="Your name.."
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
                     />
 
                     <input
@@ -198,6 +86,7 @@ export default function App() {
                 <Thankyou
                   setIsModalOpen={setIsModalOpen}
                   setIsThankyouOpen={setIsThankyouOpen}
+                  name={name}
                 />
               )}
               <img
@@ -212,20 +101,107 @@ export default function App() {
   );
 }
 
-function Thankyou({ setIsModalOpen, setIsThankyouOpen }) {
+function Heading({ setIsModalOpen }) {
   return (
-    <div>
-      <button
-        className="btn__closing-modal"
-        onClick={() => {
-          setIsThankyouOpen(false);
-          setIsModalOpen(false);
-        }}
-      >
-        Close
-      </button>
-      <p className="thankyou__text">Thank You!</p>
-    </div>
+    <section className="heading">
+      <div className="logo__box">
+        <img src="../public/logo.svg" className="logo" alt="logo" />
+        <button className="btn btn--black" onClick={() => setIsModalOpen(true)}>
+          Free Consultation
+        </button>
+      </div>
+      <div className="header__box">
+        <h1 className="header">Design solutions made easy</h1>
+        <p className="text">
+          With over ten years of experience in various design disciplines,
+          I&apos;m your one-stop shop for your design needs.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Grid() {
+  return (
+    <section className="grid">
+      <div className="grid--el grid--1">
+        <img
+          className="grid__img"
+          src="../public/pattern-graphic-design.svg"
+          alt="image"
+        />
+        <p className="grid__text">Graphic Design</p>
+      </div>
+
+      <div className="grid--el grid--2">
+        <img
+          className="grid__img"
+          src="../public/pattern-ui-ux.svg"
+          alt="image"
+        />
+        <p className="grid__text">UI/UX</p>
+      </div>
+
+      <div className="grid--el grid--3">
+        <img
+          className="grid__img"
+          src="../public/pattern-apps.svg"
+          alt="image"
+        />
+        <p className="grid__text">Apps</p>
+      </div>
+
+      <div className="grid--el grid--4">
+        <img
+          className="grid__img"
+          src="../public/pattern-illustrations.svg"
+          alt="image"
+        />
+        <p className="grid__text">Illustrations</p>
+      </div>
+
+      <div className="grid--el grid--5">
+        <img
+          className="grid__img"
+          src="../public/pattern-photography.svg"
+          alt="image"
+        />
+        <p className="grid__text">Photography</p>
+      </div>
+
+      <div className="grid--el grid--6">
+        <img
+          className="grid__img"
+          src="../public/pattern-motion-graphics.svg"
+          alt="image"
+        />
+        <p className="grid__text">Motion Graphics</p>
+      </div>
+    </section>
+  );
+}
+
+function AboutAmy({ setIsModalOpen }) {
+  return (
+    <section className="amy__about">
+      <img src="../public/image-amy.webp" alt="amy img" className="amy__img" />
+      <div className="amy__text-box">
+        <h2 className="amy__heading">
+          I&apos;m Amy, and I&apos;d love to work on your next project
+        </h2>
+        <p className="amy__text">
+          I love working with others to create beautiful design solutions.
+          I&apos;ve designed everything from brand illustrations to complete
+          mobile apps. I&apos;m also handy with a camera!
+        </p>
+        <button
+          className="btn btn--orange"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Free Consultation
+        </button>
+      </div>
+    </section>
   );
 }
 
@@ -247,23 +223,20 @@ function Slider({ slides }) {
       <h2 className="slider__heading">My Work</h2>
 
       <div className="slider__box">
-        <div
-          style={{
-            transform: `translateX(-${currentSlide * 100}%)`,
-            display: "flex",
-          }}
-        >
-          {slides?.map((img, index) => {
-            return (
-              <img
-                key={index}
-                src={img.src}
-                alt={img.alt}
-                className="slider__img"
-              />
-            );
-          })}
-        </div>
+        {slides?.map((img, index) => {
+          return (
+            <img
+              style={{
+                transform: `translateX(-${currentSlide * 100}%)`,
+                display: "flex",
+              }}
+              key={index}
+              src={img.src}
+              alt={img.alt}
+              className="slider__img"
+            />
+          );
+        })}
       </div>
 
       <div className="buttons__box">
@@ -275,5 +248,51 @@ function Slider({ slides }) {
         </button>
       </div>
     </section>
+  );
+}
+
+function Cta({ setIsModalOpen }) {
+  return (
+    <section className="cta">
+      <div className="cta__box">
+        <div className="cta__text-box">
+          <h2 className="cta__heading">Book a call with me</h2>
+          <p className="cta__text">
+            I&apos;d love to have a chat to see how I can help you. The best
+            first step is for us to discuss your project during a free
+            consultation. Then we can move forward from there.
+          </p>
+        </div>
+        <button
+          className="btn btn--orange"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Free Consultation
+        </button>
+      </div>
+      <div className="logo__box">
+        <img src="../public/logo.svg" className="logo" alt="logo" />
+        <button className="btn btn--black" onClick={() => setIsModalOpen(true)}>
+          Free Consultation
+        </button>
+      </div>
+    </section>
+  );
+}
+
+function Thankyou({ setIsModalOpen, setIsThankyouOpen, name }) {
+  return (
+    <div>
+      <button
+        className="btn__closing-modal"
+        onClick={() => {
+          setIsThankyouOpen(false);
+          setIsModalOpen(false);
+        }}
+      >
+        Close
+      </button>
+      <p className="thankyou__text">Thank You, {name}!</p>
+    </div>
   );
 }
